8ff92edcdb5b65247f5816bfe0817057fb5cadd3
Buddy Agyin <agyin3@gmail.com>
2020-01-21 21:30:27 -0700

    add postRouter routes and custom middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,55 @@
 const express = require('express');
-
+const Posts = require('./postDb.js')
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.use('/:id', validatePostId)
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Posts.get()
+    res.status(200).json(posts)
+  }catch(error) {
+    res.status(500).json({errorMessage: 'There was an error with your request'})
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  res.status(200).json(req.post)
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Posts.remove(req.params.id)
+    .then(deleted => {
+      res.status(200).json({message: 'Post successfully deleted', post: {...req.post}})
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: 'There was an error with your request'})
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Posts.update(req.params.id, req.body)
+    res.status(201).json({message: 'Post successfully updated', post: {id: req.params.id, ...req.body}})
+  }catch(error) {
+    res.status(500).json({errorMessage: 'There was an error with your request'})
+  }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+async function validatePostId(req, res, next) {
+  try {
+    const post = await Posts.getById(req.params.id)
+    if(post) {
+      req.post = post
+      next()
+    }else{
+      res.status(400).json({message: 'Invalid post id'})
+    }
+  }catch(error) {
+    res.status(500).json({errorMessage: 'There was an error with your request'})
+  }
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const helmet = require('helmet')
 const server = express();
 const userRouter = require('./users/userRouter.js')
+const postRouter = require('./posts/postRouter.js')
 
 server.use(express.json())
 server.use(logger)
 server.use(helmet())
+server.use('/api/posts', postRouter)
 server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
