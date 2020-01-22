db388ad6900ebb0dc379dee6f146555ea47e1594
Buddy Agyin <agyin3@gmail.com>
2020-01-21 20:52:26 -0700

    add helmet middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const helmet = require('helmet')
 const server = express();
 const userRouter = require('./users/userRouter.js')
 
 server.use(express.json())
 server.use(logger)
+server.use(helmet())
 server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
